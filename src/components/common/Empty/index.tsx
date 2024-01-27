2c259f70361f02b406d2441c084baa807dc31d92
hazem emad <hazem.ebrahim@ejada.com>
2024-01-27 19:08:51 +0200

    style(empty-component): aligntext center at empty

--- src/components/common/Empty/index.tsx
+++ src/components/common/Empty/index.tsx
@@ -1,10 +1,15 @@
 import React, {FC, memo} from 'react';
 import {useTranslation} from 'react-i18next';
-import {Text} from 'react-native';
+import {StyleSheet, Text} from 'react-native';
 
 const Empty: FC<{loading: boolean}> = ({loading}) => {
   const [t] = useTranslation();
-  return !loading ? <Text>{t('common.empty')}</Text> : null;
+  return !loading ? <Text style={style.text}>{t('common.empty')}</Text> : null;
 };
 
 export default memo(Empty);
+const style = StyleSheet.create({
+  text: {
+    alignSelf: 'center',
+  },
+});
